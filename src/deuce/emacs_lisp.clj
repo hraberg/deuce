3d5ee87ccf71f828e09a8319d973c0275a7d6300
Hakan Raberg <hraberg@yahoo.se>
2012-04-17 21:11:28 +0100

    special forms in their own file

---
+++ src/deuce/emacs_lisp.clj
@@ -0,0 +1,211 @@
+(ns deuce.emacs-lisp
+    (require [clojure.core :as c])
+    (require [clojure.walk :as walk])
+    (:refer-clojure :exclude [defmacro if and or cond let while]))
+
+(def t true)
+
+(c/defmacro defun
+  "Define NAME as a function.
+  The definition is (lambda ARGLIST [DOCSTRING] BODY...).
+  See also the function `interactive'."
+  [name arglist & body]
+  (c/let [[docstring body] (split-with string? body)
+          name (if (seq? name) (eval name) name)
+          arglist (replace '{&rest &} arglist)
+          [arglist &optional optional-args] (partition-by '#{&optional} arglist)
+          arglist (concat arglist (when &optional ['& (vec optional-args)]))]
+         `(do (defn ~name ~(vec arglist) ~@body)
+              (alter-meta! (var ~name) merge {:doc ~(apply str docstring)})
+              ~name)))
+
+(c/defmacro unwind-protect
+  "Do BODYFORM, protecting with UNWINDFORMS.
+  If BODYFORM completes normally, its value is returned
+  after executing the UNWINDFORMS.
+  If BODYFORM exits nonlocally, the UNWINDFORMS are executed anyway."
+  [bodyform & unwindforms])
+
+(c/defmacro condition-case
+  "Regain control when an error is signaled.
+  Executes BODYFORM and returns its value if no error happens.
+  Each element of HANDLERS looks like (CONDITION-NAME BODY...)
+  where the BODY is made of Lisp expressions."
+   [var bodyform & handlers])
+
+(c/defmacro cond
+  "Try each clause until one succeeds.
+  Each clause looks like (CONDITION BODY...).  CONDITION is evaluated
+  and, if the value is non-nil, this clause succeeds:
+  then the expressions in BODY are evaluated and the last one's
+  value is the value of the cond-form.
+  If no clause succeeds, cond returns nil.
+  If a clause has one element, as in (CONDITION),
+  CONDITION's value if non-nil is returned from the cond-form."
+  [& clauses])
+
+(c/defmacro setq
+  "Set each SYM to the value of its VAL.
+  The symbols SYM are variables; they are literal (not evaluated).
+  The values VAL are expressions; they are evaluated.
+  Thus, (setq x (1+ y)) sets `x' to the value of `(1+ y)'.
+  The second VAL is not computed until after the first SYM is set, and so on;
+  each VAL can use the new value of variables set earlier in the `setq'.
+  The return value of the `setq' form is the value of the last VAL."
+  [& sym-vals])
+
+(c/defmacro quote
+  "Return the argument, without evaluating it.  `(quote x)' yields `x'."
+  [arg])
+
+(c/defmacro let
+  "Bind variables according to VARLIST then eval BODY.
+  The value of the last form in BODY is returned.
+  Each element of VARLIST is a symbol (which is bound to nil)
+  or a list (SYMBOL VALUEFORM) (which binds SYMBOL to the value of VALUEFORM).
+  All the VALUEFORMs are evalled before any symbols are bound."
+  [varlist & body])
+
+(c/defmacro save-restriction
+  "Execute BODY, saving and restoring current buffer's restrictions.
+  The buffer's restrictions make parts of the beginning and end invisible.
+  (They are set up with `narrow-to-region' and eliminated with `widen'.)
+  This special form, `save-restriction', saves the current buffer's restrictions
+  when it is entered, and restores them when it is exited.
+  So any `narrow-to-region' within BODY lasts only until the end of the form.
+  The old restrictions settings are restored
+  even in case of abnormal exit (throw or error)."
+  [& body])
+
+(c/defmacro defconst
+  "Define SYMBOL as a constant variable.
+  The intent is that neither programs nor users should ever change this value.
+  Always sets the value of SYMBOL to the result of evalling INITVALUE.
+  If SYMBOL is buffer-local, its default value is what is set;
+   buffer-local values are not affected.
+  DOCSTRING is optional."
+  [symbol initvalue & [docstring]])
+
+(c/defmacro prog2
+  "Eval FORM1, FORM2 and BODY sequentially; return value from FORM2.
+  The value of FORM2 is saved during the evaluation of the
+  remaining args, whose values are discarded."
+  [form1 form2 & body])
+
+(c/defmacro prog1
+  "Eval FIRST and BODY sequentially; return value from FIRST.
+  The value of FIRST is saved during the evaluation of the remaining args,
+  whose values are discarded."
+  [first & body])
+
+(c/defmacro setq-default
+  "Set the default value of variable VAR to VALUE.
+  VAR, the variable name, is literal (not evaluated);
+  VALUE is an expression: it is evaluated and its value returned.
+  The default value of a variable is seen in buffers
+  that do not have their own values for the variable."
+  [& var-values])
+
+(c/defmacro save-window-excursion
+  "Execute BODY, preserving window sizes and contents.
+  Return the value of the last form in BODY.
+  Restore which buffer appears in which window, where display starts,
+  and the value of point and mark for each window.
+  Also restore the choice of selected window.
+  Also restore which buffer is current.
+  Does not restore the value of point in current buffer."
+  [& body])
+
+(c/defmacro save-excursion
+  "Save point, mark, and current buffer; execute BODY; restore those things.
+  Executes BODY just like `progn'.
+  The values of point, mark and the current buffer are restored
+  even in case of abnormal exit (throw or error).
+  The state of activation of the mark is also restored."
+  [& body])
+
+(c/defmacro with-output-to-temp-buffer
+  "Bind `standard-output' to buffer BUFNAME, eval BODY, then show that buffer."
+  [bufname & body])
+
+(c/defmacro or
+  "Eval args until one of them yields non-nil, then return that value.
+  The remaining args are not evalled at all.
+  If all args return nil, return nil."
+  [& conditions])
+
+(c/defmacro interactive
+  "Specify a way of parsing arguments for interactive use of a function.
+  For example, write
+   (defun foo (arg buf) \"Doc string\" (interactive \"P\\nbbuffer: \") .... )
+   to make ARG be the raw prefix argument, and set BUF to an existing buffer,
+   when `foo' is called as a command.
+  The \"call\" to `interactive' is actually a declaration rather than a function;
+   it tells `call-interactively' how to read arguments
+   to pass to the function.
+  When actually called, `interactive' just returns nil."
+  [& args])
+
+(c/defmacro while
+  "If TEST yields non-nil, eval BODY... and repeat.
+  The order of execution is thus TEST, BODY, TEST, BODY and so on
+  until TEST returns nil."
+  [test & body])
+
+(c/defmacro defmacro
+  "Define NAME as a macro.
+  The actual definition looks like
+   (macro lambda ARGLIST [DOCSTRING] [DECL] BODY...).
+  When the macro is called, as in (NAME ARGS...),
+  the function (lambda ARGLIST BODY...) is applied to
+  the list ARGS... as it appears in the expression,
+  and the result should be a form to be evaluated instead of the original."
+  [name arglist & body])
+
+(c/defmacro save-current-buffer
+  "Save the current buffer; execute BODY; restore the current buffer.
+  Executes BODY just like `progn'."
+  [& body])
+
+(c/defmacro function
+  "Like `quote', but preferred for objects which are functions.
+  In byte compilation, `function' causes its argument to be compiled.
+  `quote' cannot do that."
+  [arg])
+
+(c/defmacro and
+  "Eval args until one of them yields nil, then return nil.
+  The remaining args are not evalled at all.
+  If no arg yields nil, return the last arg's value."
+  [& conditions])
+
+(c/defmacro progn
+  "Eval BODY forms sequentially and return value of last one."
+  [& body])
+
+(c/defmacro let*
+  "Bind variables according to VARLIST then eval BODY.
+  The value of the last form in BODY is returned.
+  Each element of VARLIST is a symbol (which is bound to nil)
+  or a list (SYMBOL VALUEFORM) (which binds SYMBOL to the value of VALUEFORM).
+  Each VALUEFORM can refer to the symbols already bound by this VARLIST."
+  [varlist & body])
+
+(c/defmacro defvar
+  "Define SYMBOL as a variable, and return SYMBOL.
+  You are not required to define a variable in order to use it,
+  but the definition can supply documentation and an initial value
+  in a way that tags can recognize."
+  [symbol & [initvalue docstring]])
+
+(c/defmacro catch
+  "Eval BODY allowing nonlocal exits using `throw'.
+  TAG is evalled to get the tag to use; it must not be nil."
+  [tag & body])
+
+(c/defmacro if
+  "If COND yields non-nil, do THEN, else do ELSE...
+  Returns the value of THEN or the value of the last of the ELSE's.
+  THEN must be one expression, but ELSE... can be zero or more expressions.
+  If COND yields nil, and there are no ELSE's, the value is nil."
+  [cond then & else])
