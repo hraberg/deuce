36444461a40d0c046c9350aa55b00f66d17fee95
Hakan Raberg <hraberg@yahoo.se>
2012-04-22 13:42:37 +0100

    quoted strings

--- test/deuce/test/core.clj
+++ test/deuce/test/core.clj
@@ -8,19 +8,22 @@
   (is false "No tests have been written."))
 
 (defn tokenize [sc]
-  (let [look #(.findWithinHorizon sc (pr-str %) 1)
+  (let [find #(.findWithinHorizon sc %1 %2)
+        look #(find (pr-str %) 1)
         tokenize-all #(take-while identity (repeatedly (partial tokenize sc)))
         unquote #(if (look \@) 'unquote-splicing 'unquote)
-        re-sym #"[\w\d-+/*<>=.]+"]
+        re-sym #"[\w\d-+/*<>=.]+"
+        re-str #"(?:[^\"\\]|\\.)*\""]
     (cond
      (look \s) (recur sc)
      (look \() (apply list (tokenize-all))
      (look \[) (vec (tokenize-all))
      (look \,) (list (unquote) (tokenize sc))
+     (look \") (->> (find re-str 0) drop-last (remove #{\\}) (apply str))
      (.hasNextBigDecimal sc) (.nextBigDecimal sc)
      (.hasNext sc re-sym) (symbol (.next sc re-sym))
      (.hasNext sc) (not-empty (.next sc)))))
 
 (defn parse [r]
   (tokenize (doto (Scanner. (if (string? r) (StringReader. r) r))
-              (.useDelimiter #"(\ +|\]|\))"))))
+              (.useDelimiter #"( +|\]|\))"))))
