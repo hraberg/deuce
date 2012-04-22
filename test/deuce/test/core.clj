0789165895bfbb9f3a703594907543b02a87dbe8
Hakan Raberg <hraberg@yahoo.se>
2012-04-22 21:24:47 +0100

    going back to regex literals. number/radix reader macros

--- test/deuce/test/core.clj
+++ test/deuce/test/core.clj
@@ -10,7 +10,7 @@
 (declare tokenize)
 
 (def ^Pattern re-sym #"[\S&&[^~`#\'\"]]+")
-(def ^Pattern re-str #"(?:[^\"\\]|\\.)*\"")
+(def ^Pattern re-str #"(?:[^\\\"]|\\.)*\"")
 (def ^Pattern re-chr #"[\S&&[^\]\)\s]]*")
 
 (defn tokenize-all [^Scanner sc]
@@ -18,25 +18,31 @@
 
 (defn tokenize [^Scanner sc]
   (let [find (fn [^Pattern re h] (.findWithinHorizon sc re (int h)))
-        look #(find (re-pattern (pr-str %)) 1)
-        unquote #(if (look \@) 'unquote-splicing 'unquote)]
+        look #(find % 1)
+        unquote #(if (look #"@") 'unquote-splicing 'unquote)]
     (cond
-     (look \s) (recur sc)
-     (look \() (apply list (tokenize-all sc))
-     (look \[) (vec (tokenize-all sc))
-     (or (look \)) (look \])) nil
-     (look \,) (list (unquote) (tokenize sc))
-     (look \') (list 'quote (tokenize sc))
-     (look \`) (list 'syntax-quote (tokenize sc))
-     (look \:) (keyword (tokenize sc))
-     (look \?) (symbol (str \? (find re-chr 0)))
-     (look \") (string/replace (->> (find re-str 0) drop-last (apply str))
-                               #"\\(.)" "$1")
-     (look \;) (list 'comment (.nextLine sc))
-     (look \#) (if (look \')
-                 (list 'var (tokenize sc))
-                 (let [[object start end properties] (tokenize sc)]
-                   (list 'set-text-properties start end properties object)))
+     (look #"\s") (recur sc)
+     (look #"\(") (apply list (tokenize-all sc))
+     (look #"\[") (vec (tokenize-all sc))
+     (look #"[)\]]") nil
+     (look #",") (list (unquote) (tokenize sc))
+     (look #"'") (list 'quote (tokenize sc))
+     (look #"`") (list 'syntax-quote (tokenize sc))
+     (look #":") (keyword (tokenize sc))
+     (look #"\?") (symbol (str \? (find re-chr 0)))
+     (look #"\"") (string/replace (->> (find re-str 0) drop-last (apply str))
+                                  #"\\(.)" "$1")
+     (look #";") (list 'comment (.nextLine sc))
+     (look #"#") (cond
+                  (look #"'") (list 'var (tokenize sc))
+                  (look #"x") (.nextInt sc (int 16))
+                  (look #"o") (.nextInt sc (int 8))
+                  (look #"b") (.nextInt sc (int 2))
+                  (.hasNext sc #"\d+r\S+") (let [radix (find #"\d+" 0)]
+                                             (look #"r")
+                                             (.nextInt sc (Integer/parseInt radix)))
+                (look #"\(") (let [[object start end properties] (tokenize-all sc)]
+                            (list 'set-text-properties start end properties object)))
      (.hasNextLong sc) (.nextLong sc)
      (.hasNextDouble sc) (.nextDouble sc)
      (.hasNext sc re-sym) (symbol (.next sc re-sym))
