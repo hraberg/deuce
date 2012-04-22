d9a202c157c2a448b025cf7578f202e3f9498c21
Hakan Raberg <hraberg@yahoo.se>
2012-04-22 15:30:31 +0100

    making symbols very lenient to get stuff in there to start with

--- test/deuce/test/core.clj
+++ test/deuce/test/core.clj
@@ -15,16 +15,20 @@
   (let [find #(.findWithinHorizon sc %1 %2)
         look #(find (pr-str %) 1)
         unquote #(if (look \@) 'unquote-splicing 'unquote)
-        re-sym #"[\w\d-+/*<>=\.]+"
+        re-sym #"[\w\d-+/*<>=\.?\\@]+"
         re-str #"(?:[^\"\\]|\\.)*\""]
     (cond
      (look \s) (recur sc)
      (look \() (apply list (tokenize-all sc))
      (look \[) (vec (tokenize-all sc))
      (look \,) (list (unquote) (tokenize sc))
+     (look \') (list 'quote (tokenize sc))
+     (look \`) (list 'syntax-quote (tokenize sc))
+     (look \:) (keyword (tokenize sc))
      (look \") (string/replace (->> (find re-str 0) drop-last (apply str))
                                #"\\(.)" "$1")
      (look \;) (list 'comment (.nextLine sc))
+     (and (look \#) (look \')) (list 'var (tokenize sc))
      (.hasNextLong sc) (.nextLong sc)
      (.hasNextDouble sc) (.nextDouble sc)
      (.hasNext sc re-sym) (symbol (.next sc re-sym))
