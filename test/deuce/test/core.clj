742fd853a6ccc525a865e9243da789aeb1b26e4e
Hakan Raberg <hraberg@yahoo.se>
2012-04-22 14:50:35 +0100

    refactoring

--- test/deuce/test/core.clj
+++ test/deuce/test/core.clj
@@ -8,24 +8,28 @@
 (deftest replace-me ;; FIXME: write
   (is false "No tests have been written."))
 
+(defn tokenize-all [sc]
+  (take-while identity (repeatedly (partial tokenize sc))))
+
 (defn tokenize [sc]
   (let [find #(.findWithinHorizon sc %1 %2)
         look #(find (pr-str %) 1)
-        tokenize-all #(take-while identity (repeatedly (partial tokenize sc)))
         unquote #(if (look \@) 'unquote-splicing 'unquote)
-        re-sym #"[\w\d-+/*<>=.]+"
+        re-sym #"[\w\d-+/*<>=\.]+"
         re-str #"(?:[^\"\\]|\\.)*\""]
     (cond
      (look \s) (recur sc)
-     (look \() (apply list (tokenize-all))
-     (look \[) (vec (tokenize-all))
+     (look \() (apply list (tokenize-all sc))
+     (look \[) (vec (tokenize-all sc))
      (look \,) (list (unquote) (tokenize sc))
      (look \") (string/replace (->> (find re-str 0) drop-last (apply str))
                                #"\\(.)" "$1")
-     (.hasNextBigDecimal sc) (.nextBigDecimal sc)
+     (look \;) (list 'comment (.nextLine sc))
+     (.hasNextLong sc) (.nextLong sc)
+     (.hasNextDouble sc) (.nextDouble sc)
      (.hasNext sc re-sym) (symbol (.next sc re-sym))
      (.hasNext sc) (not-empty (.next sc)))))
 
 (defn parse [r]
-  (tokenize (doto (Scanner. (if (string? r) (StringReader. r) r))
-              (.useDelimiter #"( +|\]|\))"))))
+  (tokenize-all (doto (Scanner. (if (string? r) (StringReader. r) r))
+                  (.useDelimiter #"(\s+|\]|\))"))))
