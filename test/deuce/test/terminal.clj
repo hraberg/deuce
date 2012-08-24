284ac9425265deb6245bccdcdb201a98f5a71755
Hakan Raberg <hraberg@yahoo.se>
2012-08-24 09:45:19 +0100

    It's the mode line, stupid

--- test/deuce/test/terminal.clj
+++ test/deuce/test/terminal.clj
@@ -6,7 +6,7 @@
 ;;   lein trampoline run -m deuce.test.terminal
 
 ;; Press Ctrl-x-c (Ctrl-c seems to be captured) to exit. M-x for minibuffer
-;; You can type text (insert mode) and move the cursor around. The "minibuffer" line number changes.
+;; You can type text (insert mode) and move the cursor around. The line number changes on the mode line.
 
 ;; This is *NOT* Emacs, Deuce or anything close like it, even though it looks like it at first sight.
 ;; It is meant to ensure that Lanterna can handle the UI updates Emacs requires.
