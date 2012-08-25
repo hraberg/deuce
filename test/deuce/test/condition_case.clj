f75c61bff4226100d18b151b74cae1788c4911db
Hakan Raberg <hraberg@yahoo.se>
2012-08-25 12:29:01 +0100

    Wrong references

--- test/deuce/test/condition_case.clj
+++ test/deuce/test/condition_case.clj
@@ -1,13 +1,13 @@
 (ns deuce.test.condition-case
   (use [deuce.test.common]))
 
-;; "10.5.2 Examples of catch and throw"[1]
+;; "10.5.3.3 Writing Code to Handle Errors"[1]
 
 ;; Also forced out a version of / for Emacs, which rounds depending on the argument.
 ;; Hints that the scaffolded primitives should be Clojure defns, not Emacs Lisp defuns.
 ;; EmacsLispError is also a bit unclear in it's use.
 
-;; [1] http://www.gnu.org/software/emacs/manual/html_node/elisp/Examples-of-Catch.html
+;; [1] http://www.gnu.org/software/emacs/manual/html_node/elisp/Handling-Errors.html
 
 (with-fresh-emacs)
 
