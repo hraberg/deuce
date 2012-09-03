09dc4788d64b22f20619779b2596c39cc48e9d78
Hakan Raberg <hraberg@yahoo.se>
2012-09-03 16:04:51 +0100

    Moving the actual parser out of tests

---
+++ src/deuce/emacs_lisp/parser.clj
@@ -0,0 +1,47 @@
+(ns deuce.emacs-lisp.parser
+  (use [clojure.test])
+  (import [java.util Scanner]
+          [java.io StringReader StreamTokenizer]
+          [java.util.regex Pattern]))
+
+(declare tokenize)
+
+(def ^:private ^Pattern re-str #"(?s)([^\"\\]*(?:\\.[^\"\\]*)*)\"")
+
+(defn ^:private tokenize-all [^Scanner sc]
+  (take-while identity (repeatedly (partial tokenize sc))))
+
+(defn ^:private tokenize [^Scanner sc]
+  (let [find (fn [^Pattern re h] (.findWithinHorizon sc re (int h)))]
+    (condp find 1
+      #"\s" (recur sc)
+      #"[)\]]" nil
+      #"\(" (apply list (tokenize-all sc))
+      #"\[" (vec (tokenize-all sc))
+      #"," (list (if (find #"@" 1) (symbol "\\,@") (symbol "\\,")) (tokenize sc))
+      #"'" (list 'quote (tokenize sc))
+      #"`" (list '\` (tokenize sc))
+      #":" (keyword (.next sc))
+      #"\?" (symbol (str \? (.next sc)))
+      #"\"" (.sval (doto (StreamTokenizer. (StringReader. (str \" (find re-str 0))))
+                     (.nextToken)))
+      #";" (list 'comment (.nextLine sc))
+      #"#" (condp find 1
+             #"'" (list 'var (tokenize sc))
+             #"\(" (let [[object start end properties] (tokenize-all sc)]
+                     (list 'set-text-properties start end properties object))
+             #"x" (.nextInt sc (int 16))
+             #"o" (.nextInt sc (int 8))
+             #"b" (.nextInt sc (int 2))
+             (when (.hasNext sc #"\d+r\S+")
+               (let [radix (find #"\d+" 0)]
+                 (find #"r" 1)
+                 (.nextInt sc (Integer/parseInt radix)))))
+      (cond
+       (.hasNextLong sc) (.nextLong sc)
+       (.hasNextDouble sc) (.nextDouble sc)
+       (.hasNext sc) (symbol (.next sc))))))
+
+(defn parse [r]
+  (tokenize-all (doto (if (string? r) (Scanner. r) (Scanner. r "UTF-8"))
+                  (.useDelimiter #"(\s+|\]|\)|\"|;)"))))
--- test/deuce/test/parser.clj
+++ test/deuce/test/parser.clj
@@ -1,58 +1,13 @@
 (ns deuce.test.parser
-  (use [clojure.test])
-  (require [clojure.string :as string]
-           [clojure.java.io :as io])
-  (import [java.util Scanner]
-          [java.io StringReader StreamTokenizer]
-          [java.util.regex Pattern]))
+  (use [clojure.test]
+       [deuce.emacs-lisp.parser])
+  (require [clojure.java.io :as io]))
 
 ;; This is an Emacs Lisp parser spike.
 
 ;;   lein run -m deuce.test.parser
 
 ;; It can read all the .el files under emacs/lisp, but the actual representation as Clojure forms will probably change.
-
-(declare tokenize)
-
-(def ^Pattern re-str #"(?s)([^\"\\]*(?:\\.[^\"\\]*)*)\"")
-
-(defn tokenize-all [^Scanner sc]
-  (take-while identity (repeatedly (partial tokenize sc))))
-
-(defn tokenize [^Scanner sc]
-  (let [find (fn [^Pattern re h] (.findWithinHorizon sc re (int h)))]
-    (condp find 1
-      #"\s" (recur sc)
-      #"[)\]]" nil
-      #"\(" (apply list (tokenize-all sc))
-      #"\[" (vec (tokenize-all sc))
-      #"," (list (if (find #"@" 1) (symbol "\\,@") (symbol "\\,")) (tokenize sc))
-      #"'" (list 'quote (tokenize sc))
-      #"`" (list '\` (tokenize sc))
-      #":" (keyword (.next sc))
-      #"\?" (symbol (str \? (.next sc)))
-      #"\"" (.sval (doto (StreamTokenizer. (StringReader. (str \" (find re-str 0))))
-                     (.nextToken)))
-      #";" (list 'comment (.nextLine sc))
-      #"#" (condp find 1
-             #"'" (list 'var (tokenize sc))
-             #"\(" (let [[object start end properties] (tokenize-all sc)]
-                     (list 'set-text-properties start end properties object))
-             #"x" (.nextInt sc (int 16))
-             #"o" (.nextInt sc (int 8))
-             #"b" (.nextInt sc (int 2))
-             (when (.hasNext sc #"\d+r\S+")
-               (let [radix (find #"\d+" 0)]
-                 (find #"r" 1)
-                 (.nextInt sc (Integer/parseInt radix)))))
-      (cond
-       (.hasNextLong sc) (.nextLong sc)
-       (.hasNextDouble sc) (.nextDouble sc)
-       (.hasNext sc) (symbol (.next sc))))))
-
-(defn parse [r]
-  (tokenize-all (doto (if (string? r) (Scanner. r) (Scanner. r "UTF-8"))
-                  (.useDelimiter #"(\s+|\]|\)|\"|;)"))))
 
 (defn smoke []
   (doseq [el (filter #(re-find #".el$" (str %)) (file-seq (io/file "emacs/lisp")))]
