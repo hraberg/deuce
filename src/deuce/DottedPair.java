c7bbaf0eed36a38bf2dfdbff61d21d5503990bf7
Hakan Raberg <hraberg@yahoo.se>
2012-09-09 02:23:49 +0100

    equals/hashCore for DottedPair

--- src/deuce/DottedPair.java
+++ src/deuce/DottedPair.java
@@ -8,4 +8,22 @@ public class DottedPair {
         this.car = car;
         this.cdr = cdr;
     }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DottedPair that = (DottedPair) o;
+
+        if (car != null ? !car.equals(that.car) : that.car != null) return false;
+        if (cdr != null ? !cdr.equals(that.cdr) : that.cdr != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result = car != null ? car.hashCode() : 0;
+        result = 31 * result + (cdr != null ? cdr.hashCode() : 0);
+        return result;
+    }
 }
