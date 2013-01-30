35be3c36578544e10214a93427c91edd2959b104
Hakan Raberg <hraberg@yahoo.se>
2013-01-30 01:18:55 +0100

    Clojure 1.5.0-RC4 / timbre 1.3.0

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
             :url "http://www.gnu.org/licenses/"
             :distribution :repo}
   :url "http://www.gnu.org/software/emacs/"
-  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC4"]
                  [clojure-lanterna "0.9.2"]
-                 [com.taoensso/timbre "1.2.0"]]
+                 [com.taoensso/timbre "1.3.0"]]
   :plugins [[lein-swank "1.4.5"]
             [lein-difftest "2.0.0"]
             [lein-marginalia "0.7.1"]]
