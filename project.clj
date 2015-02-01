2b93b44fafb89a0e1d5aa0b0699a49c92280b006
Håkan Råberg <hraberg@yahoo.se>
2015-02-01 22:23:10 +0000

    Removing some stuff that isn't currently used.

--- project.clj
+++ project.clj
@@ -6,11 +6,9 @@
   :url "http://www.gnu.org/software/emacs/"
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [clojure-lanterna "0.9.3"]
-                 [http-kit "2.1.19"]
                  [com.taoensso/timbre "3.3.1"]
                  [org.tcrawley/dynapath "0.2.3"]
-                 [fipp "0.5.1"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [fipp "0.5.1"]]
   :repositories {"sonatype-staging"
                  "https://oss.sonatype.org/content/groups/staging/"}
   :plugins [[lein-difftest "2.0.0"]
@@ -23,6 +21,5 @@
   :resource-paths ["emacs/lisp" "resources"]
   :jar-exclusions [#".*\.elc" #"TUTORIAL\..*"
                    #"ChangeLog.*" #"Makefile.*" #"README" #"\.gitignore"]
-  :java-source-paths ["src"]
   :jvm-opts ^:replace ["-Xss4m" "-Xmx1g"]
   :main deuce.main)
