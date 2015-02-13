4d4ef6f1a20e92dd9e247855dc8210d6f9638f7b
Håkan Råberg <hraberg@yahoo.se>
2015-02-13 00:45:30 +0000

    Wrong suffix

--- resources/public/node-deuce.js
+++ resources/public/node-deuce.js
@@ -11,8 +11,8 @@ java.classpath.push(__dirname + '/../../target/deuce-0.1.0-SNAPSHOT-standalone.j
 var Clojure = java.import('clojure.java.api.Clojure'),
     cljRequire = Clojure.varSync('clojure.core/require'),
     cljSymbol = Clojure.varSync('clojure.core/symbol'),
-    cljsReadString = Clojure.varSync('clojure.core/read-string'),
-    cljsEval = Clojure.varSync('clojure.core/eval');
+    cljReadString = Clojure.varSync('clojure.core/read-string'),
+    cljEval = Clojure.varSync('clojure.core/eval');
 
 cljRequire.invokeSync(cljSymbol.invokeSync('deuce.main'));
 Clojure.varSync('deuce.emacs.lread/load').invokeSync('deuce-loadup.el');
@@ -21,10 +21,10 @@ console.log(Clojure.varSync('deuce.emacs/emacs-version').invokeSync());
 
 module.exports.Clojure = Clojure;
 module.exports.evalClojure = function (s, f) {
-    cljsReadString.invoke(s, function (form) {
-        cljsEval.invoke(form, f);
+    cljReadString.invoke(s, function (form) {
+        cljEval.invoke(form, f);
     });
 };
 module.exports.evalClojureSync = function (s) {
-    return cljsEval.invokeSync(cljsReadString.invokeSync(s));
+    return cljEval.invokeSync(cljReadString.invokeSync(s));
 };
