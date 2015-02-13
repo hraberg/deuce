9763115d9f0ba3e02750c6ac1a89f127bb86060e
Håkan Råberg <hraberg@yahoo.se>
2015-02-13 09:52:16 +0000

    ws

--- resources/public/render-spike.js
+++ resources/public/render-spike.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function alignLineNumbers() {
         var gutterWidth = ((linesInFile.length.toString().length + 1) * fontWidth);
         setCssRule('.window.linum-mode', '{ padding-left: ' + gutterWidth + 'px; }');
-        setCssRule('.window.linum-mode .line:before ',
+        setCssRule('.window.linum-mode .line:before',
                    '{ width: ' + gutterWidth + 'px; margin-left: ' + -gutterWidth + 'px; }');
     }
 
