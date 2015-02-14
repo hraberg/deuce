f9ba061d4e6e2979025040a3eecdceeed30308d7
Håkan Råberg <hraberg@yahoo.se>
2015-02-14 19:30:32 +0000

    GapBufffer playground

---
+++ resources/public/gap.js
@@ -0,0 +1,150 @@
+#!/usr/bin/env node
+/*jslint node: true */
+
+'use strict';
+
+// not persistent, uses destructive updates to the buffer.
+function GapBuffer(s) {
+    this.buffer = s.split('');
+    this.start = 0;
+    this.end = 0;
+    this.grow();
+}
+
+GapBuffer.GROWTH_FACTOR = 0.5;
+
+GapBuffer.prototype.toString = function () {
+    return this.buffer.slice(0, this.start).concat(this.buffer.slice(this.end)).join('');
+};
+
+GapBuffer.prototype.grow = function () {
+    var size = Math.round(this.length * GapBuffer.GROWTH_FACTOR),
+        before = this.buffer.slice(0, this.start),
+        gap = [].constructor(size),
+        after = this.buffer.slice(this.end);
+
+    this.buffer = before.concat(gap.concat(after));
+    this.start = before.length;
+    this.end = before.length + gap.length;
+
+    return this;
+};
+
+Object.defineProperty(GapBuffer.prototype, "length", {
+    get: function () {
+        return this.buffer.length - (this.end - this.start);
+    }
+});
+
+// should move the gap lazily on demand on first edit after point movement.
+GapBuffer.prototype.gotoChar = function (n) {
+    var before = this.buffer.slice(0, this.start),
+        gap = this.buffer.slice(this.start, this.end),
+        after = this.buffer.slice(this.end),
+        text = before.concat(after);
+    before = text.slice(0, n);
+    after = text.slice(n);
+
+    this.buffer = before.concat(gap).concat(after);
+    this.start = n;
+    this.end = n + gap.length;
+
+    return this;
+};
+
+GapBuffer.prototype.bobp = function () {
+    return this.start === 0;
+};
+
+GapBuffer.prototype.eobp = function () {
+    return this.end === this.buffer.length;
+};
+
+GapBuffer.prototype.forwardChar = function (n) {
+    n = n || 1;
+    var direction = n > 0 ? 1 : -1;
+    n = Math.abs(n);
+    while (n > 0) {
+        if ((this.bobp() && direction < 0) || (this.eobp() && direction > 0)) {
+            break;
+        }
+        if (direction > 0) {
+            this.buffer[this.start] = this.buffer[this.end];
+            delete this.buffer[this.end];
+        } else {
+            this.buffer[this.end - 1] = this.buffer[this.start - 1];
+            delete this.buffer[this.start + direction];
+        }
+        this.start += direction;
+        this.end += direction;
+        n -= 1;
+    }
+    return this;
+};
+
+GapBuffer.prototype.backwardChar = function (n) {
+    return this.forwardChar(-n);
+};
+
+GapBuffer.prototype.insert = function (s) {
+    var i;
+    for (i = 0; i < s.length; i += 1) {
+        this.buffer[this.start] = s[i];
+        this.start += 1;
+        if (this.start === this.end) {
+            this.grow();
+        }
+    }
+    return this;
+};
+
+GapBuffer.prototype.deleteChar = function (n) {
+    n = n || 1;
+    var direction = n > 0 ? 1 : -1;
+    n = Math.abs(n);
+    while (n > 0) {
+        if ((this.bobp() && direction < 0) || (this.eobp() && direction > 0)) {
+            break;
+        }
+        if (direction > 0) {
+            delete this.buffer[this.end];
+            this.end += direction;
+        } else {
+            delete this.buffer[this.start];
+            this.start += direction;
+        }
+        n -= 1;
+    }
+    return this;
+};
+
+GapBuffer.prototype.backwardDeleteChar = function (n) {
+    return this.deleteChar(-n);
+};
+
+var buffer = new GapBuffer('Hello World');
+console.log(buffer, buffer.length, buffer.toString());
+buffer = buffer.forwardChar(2);
+console.log(buffer);
+buffer = buffer.forwardChar(4);
+console.log(buffer);
+buffer = buffer.backwardChar(4);
+console.log(buffer);
+
+buffer = buffer.forwardChar(5);
+console.log(buffer);
+
+buffer = buffer.insert('Space!');
+console.log(buffer);
+
+buffer = buffer.deleteChar(2);
+console.log(buffer);
+
+buffer = buffer.backwardDeleteChar(3);
+console.log(buffer);
+
+buffer = buffer.forwardChar(50);
+console.log(buffer);
+
+buffer = buffer.gotoChar(5);
+console.log(buffer);
