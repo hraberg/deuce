9ff7a0354682f6d95b598788287d3ae4f5e82487
Håkan Råberg <hraberg@yahoo.se>
2015-02-17 16:01:37 +0000

    mmap backed Rope node experiment.

--- resources/public/rope2.js
+++ resources/public/rope2.js
@@ -1,4 +1,4 @@
-/*jslint node: true regexp: true */
+/*jslint node: true regexp: true todo: true stupid: true nomen: true */
 
 // http://citeseer.ist.psu.edu/viewdoc/download?doi=10.1.1.14.9450&rep=rep1&type=pdf
 
@@ -8,6 +8,8 @@
 // Java: https://code.google.com/p/ropes/
 // Go: https://github.com/vinzmay/go-rope
 // Scheme: https://bitbucket.org/evhan/rope
+
+// TODO: balancing, web worker?, RopeFn nodes for paging - disk (via ws?) - weak refs in JS? Performance tuning.
 
 'use strict';
 
@@ -24,11 +26,11 @@ function Rope(left, right) {
     this.memoize();
 }
 
-var RopeString;
+var RopeString, RopeFile;
 
 Rope.SHORT_LIMIT = 16;
 Rope.toRope = function (x) {
-    if (x instanceof Rope || x instanceof RopeString) {
+    if (x instanceof Rope || x instanceof RopeString || x instanceof RopeFile) {
         return x;
     }
     return new RopeString((x || '').toString());
@@ -150,14 +152,73 @@ RopeString.prototype.lineAt = function (index) {
     if (index < 0 || index > this.length) {
         return -1;
     }
-    return (this.s.slice(0, index).match(RopeString.LINES_PATTERN) || []).length;
+    return (this.toString().slice(0, index).match(RopeString.LINES_PATTERN) || []).length;
 };
 
 RopeString.prototype.indexOfLine = function (line) {
     if (line < 0 || line > this.newlines) {
         return -1;
     }
-    return this.s.match(RopeString.LINES_PATTERN).slice(0, line).join('').length;
+    return this.match(RopeString.LINES_PATTERN).slice(0, line).join('').length;
+};
+
+var fs = require('fs'),
+    mmap = require('mmap.js');
+
+// Assumes ASCII.
+
+function RopeFile(buffer, start, end) {
+    this.buffer = buffer;
+    this.start = start;
+    this.end = end;
+    this.length = end - start;
+    this.depth = 0;
+}
+
+mixin(RopeFile, String, ['charAt', 'match', 'indexOf']);
+mixin(RopeFile, Rope, ['concat', 'insert', 'del', 'lines', 'reduce']);
+mixin(RopeFile, RopeString, ['indexOfLine', 'lineAt']);
+
+RopeFile.mmap = function (file) {
+    var fd = fs.openSync(file, 'r'), buffer, length;
+    try {
+        length = fs.fstatSync(fd).size;
+        buffer = mmap.alloc(length, mmap.PROT_READ, mmap.MAP_SHARED, fd, 0);
+        return new RopeFile(buffer, 0, length);
+    } finally {
+        fs.closeSync(fd);
+    }
+};
+
+RopeFile.prototype.toString = function () {
+    return this.buffer.slice(this.start, this.end).toString();
+};
+
+RopeFile.prototype.charAt = function (index) {
+    if (index < 0 || index >= this.length) {
+        return '';
+    }
+    return String.fromCharCode(this.buffer[index + this.start]);
+};
+
+Object.defineProperty(RopeFile.prototype, 'newlines', {
+    enumerable: true,
+    get: function () {
+        if (!this._newlines) {
+            var i, acc = 0;
+            for (i = this.start; i < this.end; i += 1) {
+                if (this.charAt(i) === '\n') {
+                    acc += 1;
+                }
+            }
+            this._newlines = acc;
+        }
+        return this._newlines;
+    }
+});
+
+RopeFile.prototype.slice = function (beginSlice, endSlice) {
+    return new RopeFile(this.buffer, (beginSlice || 0) + this.start, endSlice ? endSlice + this.start : this.end);
 };
 
 var assert;
@@ -250,3 +311,23 @@ assert.deepEqual(new RopeString('HelloWorld').reduce(function (acc, x) {
     acc.push(x.toString());
     return acc;
 }, []), ['HelloWorld']);
+
+var rf = RopeFile.mmap(__dirname + '/../etc/tutorials/TUTORIAL');
+
+assert.equal(rf.charAt(0), 'E');
+assert.equal(rf.charAt(-1), '');
+assert.equal(rf.charAt(46571 + 1), '');
+assert.equal(rf.length, 46571);
+assert.equal(rf.toString().length, 46571);
+assert.equal(rf.newlines, 1122);
+assert.equal(rf.slice(6).charAt(0), 't');
+assert.equal(rf.slice(6).constructor, RopeFile);
+assert(/Emacs tutorial/.test(rf.toString()));
+assert.equal(rf.lines(2, 3), 'Emacs commands generally involve the CONTROL key (sometimes labeled\n');
+assert.equal(rf.lines(2, 3).constructor, RopeFile);
+
+assert.equal(rf.insert(2000, 'HelloWorld').length, 46571 + 10);
+assert.equal(rf.insert(2000, 'HelloWorld').constructor, Rope);
+assert.equal(rf.insert(2000, 'HelloWorld').right.constructor, RopeFile);
+assert.equal(rf.del(2000, 3000).length, 46571 - 1000);
+assert.equal(rf.del(2000, 3000).right.constructor, RopeFile);
